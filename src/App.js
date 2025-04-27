61c86e735c8e9be7a4a778ca3f6a7da33abcfff4
Berk Tenkoğlu <berk.tenkoglu@gmail.com>
2025-04-27 12:51:32 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Todolist from './Components/Todolist';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+console.log("test logu");
 function App() {
   return (
    <div className='App'>
